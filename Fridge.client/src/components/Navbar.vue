2c3bbc38faae513e9c0227ca74e3d14895093161
Auston Robertson <auston.robertson@gmail.com>
2023-06-14 14:58:27 -0600

    menu functioning

--- Fridge.client/src/components/Navbar.vue
+++ Fridge.client/src/components/Navbar.vue
@@ -1,48 +1,73 @@
 <template>
   <nav class="d-flex justify-content-center">
     <div class="d-flex flex-row justify-content-center menu">
-        <div>
-          <router-link :to="{ name: 'Pantry' }" class="btn text-success lighten-30 selectable text-uppercase">
-            PANTRY
+        
+        <div class="link">
+          <router-link :to="{ name: 'Pantry' }" class="selector">
+            <div class="link-img"> <img src="../assets/img/ingredients.svg" alt="pantry"> </div>
+            <div class="link-text"> PANTRY </div>
           </router-link>
         </div>
-        <div>
-          <router-link :to="{ name: 'Recipes' }" class="btn text-success lighten-30 selectable text-uppercase">
-            RECIPES
+        
+        <div class="link">
+          <router-link :to="{ name: 'Recipes' }" class="selector">
+            <div class="link-img"> <img src="../assets/img/recipe.svg" alt="recipe"> </div>
+            <div class="link-text"> RECIPES </div>
           </router-link>
         </div>
+
         <div class="logo-container">
-          <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
-            <div class="d-flex flex-column align-items-center">
-              <img alt="logo" src="../assets/img/logo.svg"/>
-            </div>
+          <router-link :to="{ name: 'Home' }">
+              <img class="logo" alt="logo" src="../assets/img/logo.svg"/>
           </router-link>
         </div>
-        <div>
-          <router-link :to="{ name: 'Grocery' }" class="btn text-success lighten-30 selectable text-uppercase">
-            GROCERY
+        
+        <div class="link">
+          <router-link :to="{ name: 'Grocery' }" class="selector">
+            <div class="link-img"> <img src="../assets/img/grocery_list.svg" alt="list"> </div>
+            <div class="link-text"> GROCERY </div>
           </router-link>
         </div>
-        <div>
-          <Login />
+        
+        <div class="link">
+          <div type="button" v-if="!user.isAuthenticated" @click="login" class="selector"> 
+            <div class="link-img"> <img src="../assets/img/login.svg" alt="list"> </div>
+            <div class="link-text"> LOGIN </div>
+          </div>
+          <router-link :to="{ name: 'Account' }" class="selector">
+            <div class="link-img"> <img src="../assets/img/login.svg" alt="list"> </div>
+            <div class="link-text"> ACCOUNT </div>
+          </router-link>
         </div>
+          
     </div>
   </nav>
 </template>
 
 <script>
-import Login from './Login.vue';
+import { computed } from 'vue';
+import { AppState } from '../AppState';
+import { AuthService } from '../services/AuthService';
 export default {
   setup() {
-    return {}
+    return {
+      user: computed(() => AppState.user),
+      account: computed(() => AppState.account),
+      async login() {
+        AuthService.loginWithPopup()
+      },
+      async logout() {
+        AuthService.logout({ returnTo: window.location.origin })
+      }
+    }
   },
-  components: { Login }
 }
 </script>
 
 <style scoped>
-a:hover {
-  text-decoration: none;
+/* TODO logging in shifts margin by a little bit, fix that in a future update */
+.link .router-link-exact-active {
+  border-bottom:10px solid #FFCA4B !important ;
 }
 nav {
   padding: 1.4rem 0 0 0;
@@ -51,28 +76,61 @@ nav {
   background-position-x: center;
   background-position-y: top;
   background-repeat: repeat-x;
+  max-height: 6rem;
 }
 .menu{
   background-color: white;
-  border: 0;
-  height: auto;
+  height: inherit;
   width: 100%;
-  height: 100%;
 }
 .logo-container{
   background: black;
-  bottom: 0;
   margin-top: -4rem;
-  padding: 1.5rem 1rem;
   border-radius: 50% 50% 0 0;
   width: 20%;
   min-width: 100px;
   max-width: 140px;
+}
+.logo-container a{
+  display: flex;
+  justify-content: flex-end;
+  padding: 1.5rem 1rem 0.25rem 1rem;
+  height: 100%;
+  width: 100%;
+}
+.logo{
+ width: 100%;
+}
+.link{
+  padding: 0 0.25rem;
+  flex-grow: 1;
+  font-family: 'Oswald', sans-serif;
+}
+.link .selector{
+  display: flex;
+  border-bottom: 10px solid transparent ;
+  color: black;
+  font-weight: 600;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+}
+.link-img{
+  flex-grow: 1;
+}
+.link-img img{
+  height: 100%;
+}
+.link-text{
   flex-grow: 1;
 }
 .logo-container img{
   width: 100%;
 }
-@media screen and (min-width: 768px) {
-}
+@media screen and (max-width: 370px) { .logo-container{ display: none; } }
+@media screen and (max-width: 580px) { .logo-container{   margin-top: -3.5rem; } }
+@media screen and (max-width: 515px) { .logo-container{   margin-top: -3rem; } }
 </style>
--- Fridge.client/src/pages/AccountPage.vue
+++ Fridge.client/src/pages/AccountPage.vue
@@ -3,16 +3,24 @@
     <h1>Welcome {{ account.name }}</h1>
     <img class="rounded" :src="account.picture" alt="" />
     <p>{{ account.email }}</p>
+    <button class="btn btn-dark" @click="logout">
+      <i class="mdi mdi-logout"></i>
+      logout
+    </button>
   </div>
 </template>
 
 <script>
 import { computed } from 'vue';
 import { AppState } from '../AppState';
+import { AuthService } from '../services/AuthService';
 export default {
   setup() {
     return {
-      account: computed(() => AppState.account)
+      account: computed(() => AppState.account),
+      async logout() {
+        AuthService.logout({ returnTo: window.location.origin })
+      }
     }
   }
 }
