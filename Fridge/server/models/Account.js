745987fd11e541864820652cda14a6425d5b7d7e
Nathan Moehlmann <nathan.moehlmann@gmail.com>
2023-06-14 16:36:12 -0600

    account diet

--- Fridge/server/models/Account.js
+++ Fridge/server/models/Account.js
@@ -6,7 +6,8 @@ export const AccountSchema = new Schema(
     subs: [{ type: String, unique: true }],
     email: { type: String, lowercase: true, unique: true },
     name: { type: String, required: true },
-    picture: { type: String }
+    picture: { type: String },
+    diet: { type: String, required: false, enum: ['gluten free', 'ketogenic', 'vegetarian', 'lacto-vegetarian', 'ovo-vegetarian', 'vegan', 'pescetarian', 'paleo', 'primal', 'low FODMAP', 'whole30'] }
     // NOTE If you wish to add additional properties do so here
   },
   { timestamps: true, toJSON: { virtuals: true } }
