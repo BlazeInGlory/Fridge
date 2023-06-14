0ba5aedd98b7c1635de630e86b102de898de7c4d
Jibbles <jibbles208@gmail.com>
2023-06-14 10:52:01 -0600

    init

---
+++ Fridge/server/controllers/FoodItemController.js
@@ -0,0 +1,42 @@
+import { Auth0Provider } from "@bcwdev/auth0provider";
+import { foodItemService } from "../services/FoodItemService.js";
+import BaseController from "../utils/BaseController.js"
+
+export class FoodItemController extends BaseController {
+  constructor() {
+    super('api/pantry')
+    this.router
+    .use(Auth0Provider.getAuthorizedUserInfo)
+    .post('', this.addFood)
+    // .get('', this.findAllFoodItems)
+    // .get('/:id', this.findFoodItemById)
+  }
+
+  async addFood(req, res, next) {
+    try {
+      req.body.userId = req.userInfo.id
+      const foodItem = await foodItemService.create(req.body)
+      return res.send(foodItem)
+    }catch(error) {
+      next(error)
+    }
+  }
+
+//   async findAllFoodItems(req, res, next) {
+//     try {
+//       const foodItems = await foodItemService.findAllFoodItems()
+//       return res.send(foodItems)
+//     }catch(error) {
+//       next(error)
+//     }
+//   }
+
+//   async findFoodItemsById(req, res, next){
+//   try{
+//     const foodItem = await foodItemService.findFoodItemsById(req.params.foodId)
+//     return res.send(foodItem)
+//   }catch (error) {
+//     next(error)
+//   }
+// }
+}
