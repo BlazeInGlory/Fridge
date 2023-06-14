1710ad62876bba49d7c50ecf16d1cb5b749de183
Nathan Moehlmann <nathan.moehlmann@gmail.com>
2023-06-14 15:01:27 -0600

    find all find by delete

--- Fridge/server/controllers/FavoriteRecipesController.js
+++ Fridge/server/controllers/FavoriteRecipesController.js
@@ -4,10 +4,40 @@ import BaseController from "../utils/BaseController.js";
 
 export class FavoriteRecipesController extends BaseController {
   constructor() {
-    super('api/FavoriteRecipes')
+    super('api/recipes')
     this.router
       .use(Auth0Provider.getAuthorizedUserInfo)
       .post('', this.favoriteRecipe)
+      .get('', this.getAllFavoriteRecipes)
+      .get(':recipeId', this.getRecipeById)
+      .delete('recipeId', this.deleteFavoriteRecipe)
+  }
+  async deleteFavoriteRecipe(req, res, next) {
+    try {
+      const userId = req.userInfo.Id
+      const recipeId = req.params.recipeId
+      const recipe = await favoriteRecipesService.deleteFavoriteRecipe(recipeId, userId)
+      return res.send(recipe)
+    } catch (error) {
+      next(error)
+    }
+  }
+  async getRecipeById(req, res, next) {
+    try {
+      const recipeId = req.params.recipeId
+      const recipe = await favoriteRecipesService.getRecipeById(recipeId)
+      return res.send(recipe)
+    } catch (error) {
+      next(error)
+    }
+  }
+  async getAllFavoriteRecipes(req, res, next) {
+    try {
+      const recipes = await favoriteRecipesService.getAllFavoriteRecipes()
+      return res.send(recipes)
+    } catch (error) {
+      next(error)
+    }
   }
   async favoriteRecipe(req, res, next) {
     try {
