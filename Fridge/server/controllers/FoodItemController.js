18e1cc8ad719c2ddb0fd4ce9c8c74e42a9835346
Jibbles <jibbles208@gmail.com>
2023-06-14 12:03:43 -0600

    init

--- Fridge/server/controllers/FoodItemController.js
+++ Fridge/server/controllers/FoodItemController.js
@@ -6,40 +6,40 @@ export class FoodItemController extends BaseController {
   constructor() {
     super('api/pantry')
     this.router
-    .use(Auth0Provider.getAuthorizedUserInfo)
-    .post('', this.addFood)
-    .delete('/:foodItemId', this.archiveFood)
+      .use(Auth0Provider.getAuthorizedUserInfo)
+      .post('', this.addFood)
+      .delete('/:foodItemId', this.archiveFood)
     // .get('', this.findAllFoodItems)
     // .get('/:id', this.findFoodItemById)
   }
 
   async addFood(req, res, next) {
     try {
-      req.body.userId = req.userInfo.id
+      req.body.accountId = req.userInfo.id
       const foodItem = await foodItemService.addFood(req.body)
       return res.send(foodItem)
-    }catch(error) {
+    } catch (error) {
       next(error)
     }
   }
 
-  async archiveFood (req, res, next) {
+  async archiveFood(req, res, next) {
     try {
       const foodItem = await foodItemService.archiveFood(req.params.foodItemId, req.userInfo.userId)
       return res.send(foodItem)
-    }catch (error)  {
+    } catch (error) {
       next(error)
     }
   }
 
-    async findFoodItemsById(req, res, next){
-  try{
-    const foodItem = await foodItemService.findFoodItemsById(req.params.foodId)
-    return res.send(foodItem)
-  }catch (error) {
-    next(error)
+  async findFoodItemsById(req, res, next) {
+    try {
+      const foodItem = await foodItemService.findFoodItemsById(req.params.foodId)
+      return res.send(foodItem)
+    } catch (error) {
+      next(error)
+    }
   }
-}
 }
 
 //   async findAllFoodItems(req, res, next) {
