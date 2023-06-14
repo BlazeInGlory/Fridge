4a9fd294eb775829536216007f31af25fd7f0fba
Jibbles <jibbles208@gmail.com>
2023-06-14 10:01:36 -0600

    init

--- Fridge/server/db/DbContext.js
+++ Fridge/server/db/DbContext.js
@@ -1,10 +1,12 @@
 import mongoose from 'mongoose'
 import { AccountSchema } from '../models/Account'
 import { ValueSchema } from '../models/Value'
+import { FoodItemSchema } from "../models/FoodItem.js";
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
+  FoodItem = mongoose.model('FoodItem', FoodItemSchema);
 }
 
 export const dbContext = new DbContext()
