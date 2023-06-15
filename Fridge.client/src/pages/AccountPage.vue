823059cad3cb82c5040966cace2a146ff9ce0498
Jibbles <jibbles208@gmail.com>
2023-06-15 11:35:37 -0600

    ini

--- Fridge.client/src/pages/AccountPage.vue
+++ Fridge.client/src/pages/AccountPage.vue
@@ -1,23 +1,26 @@
 <template>
   <div class="about text-center m-5">
     <img class="rounded" :src="account.picture" alt="" />
-    <div class="card p-5">
+    <div class="card">
       <h1>Welcome {{ account.name }}</h1>
     <div class="mb-3">
-  <label for="formFile" class="form-label">Change Profile Picture?</label>
-  <input class="form-control" type="file" id="formFile">
+      <label for="url" class="p-3">Choose img from URL</label>
+<input type="url" name="url" id="url"
+       placeholder="https://example.com"
+       pattern="https://.*" size="30">
+       <button class="btn btn-success m-1">Submit</button>
 </div>
     <div class="mb-3">
   <label for="exampleFormControlInput1" class="form-label">Email Address</label>
   <input type="email" class="form-control" id="exampleFormControlInput1" placeholder={{account.email}}>
 </div>
 <div class="mb-3">
-  <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+  <label for="exampleFormControlTextarea1" class="form-label">Recipe Preferences:</label>
   <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
 </div>
 </div>
-    <button class ="btn btn-danger" @click="saveChanges">
-    <i class="mdi mdi-save"></i>Save Changes</button>
+    <button class ="btn btn-primary" @click="saveChanges">
+    <i></i>Save Changes</button>
     <button class="btn btn-dark" @click="Logout">
       <i class="mdi mdi-logout"></i>
       logout
