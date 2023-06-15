c69f13d9f3de11e8053a0a6e94f6a0e7eccedf4c
Jibbles <jibbles208@gmail.com>
2023-06-15 10:59:54 -0600

    ini

--- Fridge.client/src/pages/AccountPage.vue
+++ Fridge.client/src/pages/AccountPage.vue
@@ -1,9 +1,24 @@
 <template>
-  <div class="about text-center">
-    <h1>Welcome {{ account.name }}</h1>
+  <div class="about text-center m-5">
     <img class="rounded" :src="account.picture" alt="" />
-    <p>{{ account.email }}</p>
-    <button class="btn btn-dark" @click="logout">
+    <div class="card p-5">
+      <h1>Welcome {{ account.name }}</h1>
+    <div class="mb-3">
+  <label for="formFile" class="form-label">Change Profile Picture?</label>
+  <input class="form-control" type="file" id="formFile">
+</div>
+    <div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Email Address</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder={{account.email}}>
+</div>
+<div class="mb-3">
+  <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+</div>
+</div>
+    <button class ="btn btn-danger" @click="saveChanges">
+    <i class="mdi mdi-save"></i>Save Changes</button>
+    <button class="btn btn-dark" @click="Logout">
       <i class="mdi mdi-logout"></i>
       logout
     </button>
