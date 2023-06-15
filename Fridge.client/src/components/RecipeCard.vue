0fbce4dd651795013a8ad35a4ceaef0d6cbae61c
Auston Robertson <auston.robertson@gmail.com>
2023-06-15 15:36:43 -0600

    spoonacular working

--- Fridge.client/src/components/RecipeCard.vue
+++ Fridge.client/src/components/RecipeCard.vue
@@ -1,11 +1,14 @@
 <template>
-  Content
+  <div class="card">
+    {{ recipe.name }}
+  </div>
 </template>
   
 <script>
+import { Recipe } from '../models/Recipe'
   export default {
     props:{
-
+      recipe: {type: Recipe, required: true}
     },
     setup() {
       return {
