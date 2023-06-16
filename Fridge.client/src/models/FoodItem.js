1c3a166eeeb2ba641c82df8bf81808286d09b702
Nathan Moehlmann <nathan.moehlmann@gmail.com>
2023-06-16 09:26:29 -0600

    cool

--- Fridge.client/src/models/FoodItem.js
+++ Fridge.client/src/models/FoodItem.js
@@ -5,9 +5,7 @@ export class ApiFoodItem{
         this.quantity = data.quantity || 0
         this.unit = data.measurementUnit || data.serving_unit || 'Piece'
         this.type = data.type || ''
-        this.food_name = data.food_name
         this.serving_qty = data.serving_qty || 0
-        this.serving_unit = data.serving_unit || 'Piece'
         this.common_type = data.common_type || ''
         this.storageType = data.storageType || 'Pantry'
         this.archived = data.archived || false
