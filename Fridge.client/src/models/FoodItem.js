7bff7f72b5dc766a017e505ca58ac859f8918773
Auston Robertson <auston.robertson@gmail.com>
2023-06-16 09:24:39 -0600

    fixed undefined on model

--- Fridge.client/src/models/FoodItem.js
+++ Fridge.client/src/models/FoodItem.js
@@ -3,11 +3,11 @@ export class ApiFoodItem{
         this.foodItemId = data.foodItemId || data.tag_id
         this.name = data.name  || data.food_name
         this.quantity = data.quantity || 0
-        this.unit = data.measurementUnit 
+        this.unit = data.measurementUnit || data.serving_unit || 'Piece'
         this.type = data.type || ''
         this.food_name = data.food_name
         this.serving_qty = data.serving_qty || 0
-        this.serving_unit = data.serving_unit
+        this.serving_unit = data.serving_unit || 'Piece'
         this.common_type = data.common_type || ''
         this.storageType = data.storageType || 'Pantry'
         this.archived = data.archived || false
