996f218a4a6728cb2845a8a0ec3288d83471c635
Nathan Moehlmann <nathan.moehlmann@gmail.com>
2023-06-16 09:14:25 -0600

    adsf

--- Fridge/server/models/FoodItem.js
+++ Fridge/server/models/FoodItem.js
@@ -3,8 +3,9 @@ const ObjectId = Schema.Types.ObjectId
 
 export const FoodItemSchema = new Schema({
   accountId: { type: ObjectId, required: true, ref: 'Account' },
-  // foodItemId: { type: String, required: true },
-  // name: { type: String, required: true },
+  foodItemId: { type: String, required: true },
+  name: { type: String, required: true },
+  // NOTE i comment these out if it returns 400
   quantity: { type: Number, required: true },
   shoppingQty: { type: Number, required: false, default: 1 },
   serving_unit: { type: String, required: true },
@@ -12,7 +13,7 @@ export const FoodItemSchema = new Schema({
   storageType: { type: String, required: false, default: 'Pantry' },
   archived: { type: Boolean, required: true, default: false },
   tag_id: { type: String, required: true },
-  photo: {type: Object, required: false}
+  photo: { type: Object, required: false }
 }, { timestamps: true, toJSON: { virtuals: true } }
 )
 
