f83d5241469c462ecd7dd3f69501a72fda66dba2
Nathan Moehlmann <nathan.moehlmann@gmail.com>
2023-06-16 10:03:20 -0600

    hm

--- Fridge/server/models/FoodItem.js
+++ Fridge/server/models/FoodItem.js
@@ -8,12 +8,12 @@ export const FoodItemSchema = new Schema({
   // NOTE i comment above out if it returns 400
   quantity: { type: Number, required: true },
   shoppingQty: { type: Number, required: false, default: 1 },
-  serving_unit: { type: String, required: true },
   type: { type: String, required: false },
   storageType: { type: String, required: false, default: 'Pantry' },
   archived: { type: Boolean, required: true, default: false },
-  tag_id: { type: String, required: true },
-  photo: { type: Object, required: false }
+  photo: { type: Object, required: false },
+  // tag_id: { type: String, required: true },
+  unit: { type: String, required: true }
 }, { timestamps: true, toJSON: { virtuals: true } }
 )
 
