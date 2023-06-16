e70c725482eb9972b51a4cb16d0aba198240e862
Auston Robertson <auston.robertson@gmail.com>
2023-06-16 11:25:11 -0600

    simple edits

--- Fridge.client/src/AppState.js
+++ Fridge.client/src/AppState.js
@@ -22,61 +22,61 @@ export const AppState = reactive({
   ],
   /** @type {import('./models/FoodItem.js').FoodItem} */
   pantry: [
-    // new FoodItem({
-    //   id: 'dummyData1',
-    //   accountId: 'dummyAccount',
-    //   foodItemId: 'dummyApiData1',
-    //   name: 'apples', 
-    //   quantity: 0,
-    //   unit: 'piece',
-    //   type: 'produce',
-    //   storageType: 'Pantry',
-    //   archived: false
-    // }),
-    // new FoodItem({
-    //   id: 'dummyData2',
-    //   accountId: 'dummyAccount',
-    //   foodItemId: 'dummyApiData2',
-    //   name: 'chicken breasts', 
-    //   quantity: 3,
-    //   unit: 'piece',
-    //   type: 'meat',
-    //   storageType: 'Pantry',
-    //   archived: false
-    // }),
-    // new FoodItem({
-    //   id: 'dummyData3',
-    //   accountId: 'dummyAccount',
-    //   foodItemId: 'dummyApiData3',
-    //   name: 'Pop-Tarts', 
-    //   quantity: 2,
-    //   unit: 'piece',
-    //   type: 'produce',
-    //   storageType: 'Pantry',
-    //   archived: true
-    // }),
-    // new FoodItem({
-    //   id: 'dummyData4',
-    //   accountId: 'dummyAccount',
-    //   foodItemId: 'dummyApiData4',
-    //   name: 'Baby Bella Mushrooms', 
-    //   quantity: 6,
-    //   unit: 'oz',
-    //   type: 'vegetable',
-    //   storageType: 'Fridge',
-    //   archived: false
-    // }),
-    // new FoodItem({
-    //   id: 'dummyData5',
-    //   accountId: 'dummyAccount',
-    //   foodItemId: 'dummyApiData5',
-    //   name: 'Bell Peppers', 
-    //   quantity: 0,
-    //   unit: 'piece',
-    //   type: 'produce',
-    //   storageType: 'Fridge',
-    //   archived: true
-    // }),
+    new FoodItem({
+      id: 'dummyData1',
+      accountId: 'dummyAccount',
+      foodItemId: 'dummyApiData1',
+      name: 'apples', 
+      quantity: 0,
+      unit: 'piece',
+      type: 'produce',
+      storageType: 'Pantry',
+      archived: false
+    }),
+    new FoodItem({
+      id: 'dummyData2',
+      accountId: 'dummyAccount',
+      foodItemId: 'dummyApiData2',
+      name: 'chicken breasts', 
+      quantity: 3,
+      unit: 'piece',
+      type: 'meat',
+      storageType: 'Pantry',
+      archived: false
+    }),
+    new FoodItem({
+      id: 'dummyData3',
+      accountId: 'dummyAccount',
+      foodItemId: 'dummyApiData3',
+      name: 'Pop-Tarts', 
+      quantity: 2,
+      unit: 'piece',
+      type: 'produce',
+      storageType: 'Pantry',
+      archived: true
+    }),
+    new FoodItem({
+      id: 'dummyData4',
+      accountId: 'dummyAccount',
+      foodItemId: 'dummyApiData4',
+      name: 'Baby Bella Mushrooms', 
+      quantity: 6,
+      unit: 'oz',
+      type: 'vegetable',
+      storageType: 'Fridge',
+      archived: false
+    }),
+    new FoodItem({
+      id: 'dummyData5',
+      accountId: 'dummyAccount',
+      foodItemId: 'dummyApiData5',
+      name: 'Bell Peppers', 
+      quantity: 0,
+      unit: 'piece',
+      type: 'produce',
+      storageType: 'Fridge',
+      archived: true
+    }),
   ],
   spoonacularRecipes: null,
   favoriteRecipes: null,
--- Fridge.client/src/services/PantryService.js
+++ Fridge.client/src/services/PantryService.js
@@ -25,11 +25,12 @@ class PantryService{
 }
 
     async getMyPantry(){
-    const res = await api.get('api/pantry')
-    AppState.pantry = res.data.map( f => new FoodItem(f))
-    logger.log(AppState.pantry)
-
-}
+    // NOTE this turns off api requests when the bool is flipped in the AppState
+        if (!AppState.apiOn){ return }
+        const res = await api.get('api/pantry')
+        AppState.pantry = res.data.map( f => new FoodItem(f))
+        logger.log(AppState.pantry)
+    }
     async deleteThisFoodForever(id){
     const res = await api.delete(`api/pantry/${id}`)
     logger.log(res.data)
