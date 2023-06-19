83ab6dc37c3291fb11fe62110d348583f9b92334
Auston Robertson <auston.robertson@gmail.com>
2023-06-19 12:12:28 -0600

    filtered appstate in pantry

--- Fridge.client/src/pages/PantryPage.vue
+++ Fridge.client/src/pages/PantryPage.vue
@@ -59,7 +59,7 @@ export default {
       getMyPantry()
     })
     return {
-      pantryItems: computed(() => AppState?.pantry)
+      pantryItems: computed(() => AppState?.pantry?.filter(f => !f?.archived && f?.quantity > 0))
       // return AppState.towerEvents.filter(a => a.type == filterBy.value)
     }
   }
