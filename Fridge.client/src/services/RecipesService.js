85d1e2293198b698a395d80e0598544d637909b5
Nathan Moehlmann <nathan.moehlmann@gmail.com>
2023-06-22 23:38:10 -0600

    changes

--- Fridge.client/src/services/RecipesService.js
+++ Fridge.client/src/services/RecipesService.js
@@ -10,6 +10,7 @@ class RecipesService {
             return 
         }
         const res = await spoonacular.get(`/findByIngredients?ingredients=${ingredients}`)
+        // const res = await spoonacular.get(`/complexSearch?diet=${diets}&includeIngredients=${ingredients}&fillIngredients=true&number=15`)
         if (AppState.logging){ logger.log(res) }
         AppState.spoonacularRecipes = res.data.map( r => new Recipe(r))
         if (AppState.logging){logger.log( AppState.spoonacularRecipes) }
