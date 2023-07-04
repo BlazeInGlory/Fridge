bb2940e268d8b7504d9a49c328af4b7006fe7d3a
Auston Robertson <auston.robertson@gmail.com>
2023-07-04 12:52:31 -0600

    better styling

--- Fridge.client/src/components/NutritionixCard.vue
+++ Fridge.client/src/components/NutritionixCard.vue
@@ -1,17 +1,37 @@
 <template>
-  <div class="wid-100 d-flex flex-column" @mouseleave="closeEdits()" @mouseenter="openEdits()">
+  <div class="wid-100 d-flex flex-row justify-content-between pad-x-05 pad-y-05" @mouseleave="closeEdits()" @mouseenter="openEdits()">
 
-    <div class="card-info overflow-hidden" @click="openEdits()">
-      <div class="col-12 d-flex align-items-center gap-3 fs-2 fw-medium">
-        <img :src="food.photo" alt="">
-        <p>{{ food.name }}</p>
-      </div>
-      <div>
-        <p class="m-0 lh-1">serving unit: <span class="fw-bold">{{ food.unit }}</span></p>
+    <div class="tran-300 card-info d-flex flex-row text-nowrap overflow-hidden wid-0" 
+    @click="openEdits()"
+    :class="{'flex-grow-1':!expanded}">
+      <img :src="food.photo" :alt="food.name" class="unit-img">
+      <div class="d-flex flex-column pad-x-05">
+        <h5 class="m-1 lh-1 p-0 text-capitalize">{{ food.name }}</h5>
+        <p class="m-1 lh-1 p-0">Unit: <span class="fw-bold">{{ food.unit }}</span></p>
       </div>
     </div>
+    <div class="d-flex flex-row oswald fw-600 fs-1 align-items-center text-center justify-content-center lh-1 tran-300" 
+    :class="{active: expanded}">
+
+      <button :disabled="!food.quantity >= 1" 
+        @click="changePantryQty(-1, food.foodItemId)"
+        class="btn-subtraction spoil wid-0 text-nowrap tran-300"
+        :class="{'flex-grow-1': expanded}">
+        <i class="mdi mdi-minus"></i> 1
+      </button>
+
+      <div class="flex-grow-1">
+        {{ food.quantity }}
+      </div>
+
+      <button @click="changePantryQty(1, food.foodItemId)" 
+        class="btn-addition fresh wid-0 text-nowrap tran-300"
+        :class="{'flex-grow-1': expanded}">
+        <i class="mdi mdi-plus"></i> 1
+      </button>
+    </div>
       
-    <div class="card-edit bg-cs-white overflow-hidden tran-inout-300 d-flex flex-row justify-content-between" :class="{active: expanded}">
+    <!-- <div class="card-edit bg-cs-white overflow-hidden tran-inout-300 d-flex flex-row justify-content-between" ">
       <button :disabled="!food.quantity >= 1" 
         @click="changePantryQty(-1, food.foodItemId)"
         class="btn btn-subtraction spoil flex-grow-1">
@@ -24,7 +44,7 @@
         class="btn btn-addition fresh flex-grow-1">
         <i class="mdi mdi-plus"></i> 1
       </button>
-    </div>
+    </div> -->
 
   </div>
 </template>
@@ -64,33 +84,29 @@ import { AppState } from '../AppState'
 </script>
 
 <style scoped>
+.unit-img{
+  border-radius: 0.4rem;
+}
 .card-container{
   margin-bottom: 0.5rem;
 }
 .card-info{
   cursor: pointer;
-  border-radius: 0.4rem 0.4rem 0 0;
 }
 .card-edit{
-  /* background-color: white; */
   height: 0;
-  /* overflow: hidden; */
-  /* transition: all 200ms ease-in-out; */
   border-radius: 0 0 0.4rem 0.4rem;
 }
 .active{
-  height: 4rem;
-  padding: 0.25rem;
+  flex-grow: 1;
 }
 .btn-addition{
-  /* background-color: green; */
   border: 0;
-  border-radius: 0 0.5rem 0.5rem 0;
+  border-radius: 0.5rem;
 }
 .btn-subtraction{
-  /* background-color: tomato; */
   border: 0;
-  border-radius: 0.5rem 0 0 0.5rem;
+  border-radius: 0.5rem;
 }
 
 img {
